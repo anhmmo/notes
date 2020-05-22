2fef614227ee219f58eb771d1ca1deeb9e00b7f1
anhmmo <aun@metropolia.fi>
2020-05-22 21:15:28 +0300

    install new npm pack into project

---
+++ backend/build/precache-manifest.7a1def7e870aa33b712b265a83a56b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11aec5cab6775f16c42722329cbef073",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4706fc8e21c95acf19a5",
+    "url": "/static/css/main.cb475e6c.chunk.css"
+  },
+  {
+    "revision": "e6f8b66737d5dda4709e",
+    "url": "/static/js/2.32b5df15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.32b5df15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4706fc8e21c95acf19a5",
+    "url": "/static/js/main.401a9f26.chunk.js"
+  },
+  {
+    "revision": "420e90d25d756f6d8af3",
+    "url": "/static/js/runtime-main.e56cac76.js"
+  }
+]);
